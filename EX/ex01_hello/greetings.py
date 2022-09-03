3988b4dd59b6649f07a227ec7d7e52c007ffd3f4
Martin <markovigor99@gmail.com>
2022-09-03 17:25:13 +0300

    1 try

---
+++ EX/ex01_hello/greetings.py
@@ -0,0 +1,12 @@
+
+
+# This program writes a greeting and a recipient of your choice.
+# Then you can set the number of times greeting repeats itself.
+print("Enter a greeting")
+greeting = input()
+print("Enter a recipient")
+recipient = input()
+print("How many times to repeat")
+number = int(input())
+sum = (greeting + " " + recipient + "! ")
+print(sum * number)
