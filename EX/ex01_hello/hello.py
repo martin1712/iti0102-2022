a8fbf9d7783c63abadc1d62a332b771c745592f4
Martin <markovigor99@gmail.com>
2022-09-03 12:02:28 +0300

    1 try

---
+++ EX/ex01_hello/hello.py
@@ -0,0 +1,10 @@
+
+
+print("What is your name?")
+name = input()
+print("Hello,", name + "!" + " " + "Enter a random number:")
+num1 = int(input())
+print("Great!" + " " + "Now enter a second random number:")
+num2 = int(input())
+sum = int(num1) + int(num2)
+print('{0} + {1} is {2}'.format(num1, num2, sum))
