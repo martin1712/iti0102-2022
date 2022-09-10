55f30cb61602951e2a11eae6349c76b7aaf6f4cd
Martin <markovigor99@gmail.com>
2022-09-10 16:24:14 +0300

    2 try

--- EX/ex02_math/operators_task.py
+++ EX/ex02_math/operators_task.py
@@ -3,3 +3,31 @@
 def add(x: int, y: int):
     x + y
     return x + y
+
+def sub(x: int, y: int):
+    x - y
+    return x - y
+
+def multiply(x: int, y: int):
+    x * y
+    return x * y
+
+def div(x: int, y: int):
+    x / y
+    return x / y
+
+def modulus(x: int, y: int):
+    y % x
+    return y % x
+
+def floor_div(x: int, y: int):
+    x // y
+    return x // y
+
+def exponent(x: int, y: int):
+    x ** y
+    return x ** y
+
+def first_greater_or_equal(x: int, y: int):
+    x >= y
+    return x >= y
