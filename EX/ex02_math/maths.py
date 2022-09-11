10d026a24e538320f2d3279ffdf05090c53fb455
Martin <markovigor99@gmail.com>
2022-09-11 16:40:21 +0300

    3 try

--- EX/ex02_math/maths.py
+++ EX/ex02_math/maths.py
@@ -6,12 +6,13 @@ def average(a: int, b: int, c: int, d: int) -> float:
     b_1 = b * 2
     c_1 = c * 3
     d_1 = d * 4
-    return (a_1 + b_1 + c_1 + d_1) / 2
+    return (a_1 + b_1 + c_1 + d_1) / 4
 
 
 def school_pressure(ects: int, weeks: int) -> float:
     """School_pressure function."""
     a = ects * 26
+    weeks != 0
     return a / weeks
 
 
