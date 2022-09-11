720d8d67fb63c6520d0bfd7932b78c942d9ebfba
Martin <markovigor99@gmail.com>
2022-09-11 16:18:23 +0300

    1 try

---
+++ EX/ex02_math/maths.py
@@ -0,0 +1,22 @@
+
+
+def average(a: int, b: int, c: int, d: int):
+    """Average function."""
+    a_1 = a * 1
+    b_1 = b * 2
+    c_1 = c * 3
+    d_1 = d * 4
+    return (a_1 + b_1 + c_1 + d_1) / 2
+
+
+def school_pressure(ects: int, weeks: int):
+    """School_pressure function."""
+    a = ects * 26
+    return a / weeks
+
+
+def add_fractions(a: int, b: int, c: int, d: int):
+    """Add_fractions function."""
+    x = a / b
+    y = c / d
+    return x + y
