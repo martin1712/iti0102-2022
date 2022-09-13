fe0418cd160f69e169f21c5a2f40042be90cb84c
Martin <markovigor99@gmail.com>
2022-09-13 14:44:06 +0300

    prime end

--- EX/ex02_math/prime.py
+++ EX/ex02_math/prime.py
@@ -2,10 +2,11 @@
 
 """Is_prime_number."""
 
+
 def is_prime_number(number: int) -> bool:
     """1 and 0 are not prime numbers."""
     if number > 1:
-        for i in range(2, number):
-            if (number % i) == 0:
+        for num in range(2, number):
+            if (number % num) == 0:
                 return False
         return True
