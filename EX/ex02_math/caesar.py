ac48e0e51a78993b8f20f884a2ffbf2b9e3cd562
Martin <markovigor99@gmail.com>
2022-09-13 14:37:32 +0300

    5 try

--- EX/ex02_math/caesar.py
+++ EX/ex02_math/caesar.py
@@ -3,10 +3,13 @@
 def encode(message: str, shift: int) -> str:
     alphabet = "abcdefghijklmnopqrstuvwxyz"
     result = ""
+    ord_first_letter_lower = ord("a")
     for i in message:
         if i in alphabet:
-            result += chr(ord(i) + shift)
+            result += chr(((ord(i) - ord_first_letter_lower + shift) % 26) + ord_first_letter_lower)
         else:
             result += i
 
     return result
+
+print(encode("dog eats", 5))
