6747196eea2ba4fe94a38f77f6243980169ae617
Martin <markovigor99@gmail.com>
2022-09-13 13:26:55 +0300

    3 try

--- EX/ex02_math/caesar.py
+++ EX/ex02_math/caesar.py
@@ -5,5 +5,5 @@ def encode(message: str, shift: int) -> str:
     alphabet = "abcdefghijklmnopqrstuvwxyz"
     result = ""
     if message.isalpha():
-        for i in range(len(message):
+        for i in range(len(message)):
             result = message[i] + shift
