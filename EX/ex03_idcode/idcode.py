eec9bf8ea1f0db5ffe4fbcc0961f95fe51b53dda
Martin <markovigor99@gmail.com>
2022-09-18 17:59:17 +0300

    2 try

--- EX/ex03_idcode/idcode.py
+++ EX/ex03_idcode/idcode.py
@@ -14,14 +14,6 @@ def find_id_code(text: str) -> str:
         return result
 
 
-if __name__ == '__main__':
-    print("\nFind ID code:")
-    print(find_id_code(""))  # -> "Not enough numbers!"
-    print(find_id_code("123456789123456789"))  # -> "Too many numbers!"
-    print(find_id_code("ID code is: 49403136526"))  # -> "49403136526"
-    print(find_id_code("efs4  9   #4aw0h 3r 1a36g5j2!!6-"))  # -> "49403136526"
-
-
 def the_first_control_number_algorithm(text: str) -> str:
     result = ""
     for i in text:
@@ -35,13 +27,3 @@ def the_first_control_number_algorithm(text: str) -> str:
         return "Needs the second algorithm!"
     else:
         return result
-
-
-if __name__ == '__main__':
-    print(the_first_control_number_algorithm(""))  # -> "Incorrect ID code!"
-    print(the_first_control_number_algorithm("123456789123456789"))  # -> "Incorrect ID code!"
-    print(the_first_control_number_algorithm("ID code is: 49403136526"))  # -> "49403136526"
-    print(the_first_control_number_algorithm("efs4  9   #4aw0h 3r 1a36g5j2!!6-"))  # -> "49403136526"
-    print(the_first_control_number_algorithm("50412057633"))  # -> "50412057633"
-    print(the_first_control_number_algorithm("Peeter's ID is euf50weird2fs0fsk51ef6t0s2yr7fyf4"))  # -> "Needs
-    # the second algorithm!"
