601937b52fc0139d7f9d9b3182bae6abfe8056ac
Martin <markovigor99@gmail.com>
2022-09-18 18:28:38 +0300

    2 try

--- EX/ex03_idcode/idcode.py
+++ EX/ex03_idcode/idcode.py
@@ -22,7 +22,7 @@ def the_first_control_number_algorithm(text: str) -> str:
     a = len(result)
     if a != 11:
         return "Incorrect ID code"
-    b = int(result[0] * 1 + result[1] * 2 + result[2] * 3 + result[3] * 4 + result[4] * 5 + result[5] * 6 + result[6] * 7 + result[7] * 8 + result[8] * 9 + result[9] * 1)
+    b = int(result[0]) * 1 + int(result[1]) * 2 + int(result[2]) * 3 + int(result[3]) * 4 + int(result[4]) * 5 + int(result[5]) * 6 + int(result[6]) * 7 + int(result[7]) * 8 + int(result[8]) * 9 + int(result[9]) * 1
     c = b % 11
     if c >= 10:
         return "Needs the second algorithm!"
