fb916b1d9a6d407d89a358eb478e7f65d2eb28b2
Martin <markovigor99@gmail.com>
2022-09-18 17:57:07 +0300

    2 try

--- EX/ex03_idcode/idcode.py
+++ EX/ex03_idcode/idcode.py
@@ -30,8 +30,18 @@ def the_first_control_number_algorithm(text: str) -> str:
     a = len(result)
     if a != 11:
         return "Incorrect ID code"
-    b = result[0] * 1 + result[1] * 2 + result[2] * 3 + result[3] * 4 + result[4] * 5 + result[5] * 6 + result[6] * 7 + result[7] * 8 + result[8] * 9 + result[9] * 1
-    if int(b % 11) <= 10:
+    b = int(result[0] * 1 + result[1] * 2 + result[2] * 3 + result[3] * 4 + result[4] * 5 + result[5] * 6 + result[6] * 7 + result[7] * 8 + result[8] * 9 + result[9] * 1)
+    if b % 11 <= 10:
         return "Needs the second algorithm!"
     else:
         return result
+
+
+if __name__ == '__main__':
+    print(the_first_control_number_algorithm(""))  # -> "Incorrect ID code!"
+    print(the_first_control_number_algorithm("123456789123456789"))  # -> "Incorrect ID code!"
+    print(the_first_control_number_algorithm("ID code is: 49403136526"))  # -> "49403136526"
+    print(the_first_control_number_algorithm("efs4  9   #4aw0h 3r 1a36g5j2!!6-"))  # -> "49403136526"
+    print(the_first_control_number_algorithm("50412057633"))  # -> "50412057633"
+    print(the_first_control_number_algorithm("Peeter's ID is euf50weird2fs0fsk51ef6t0s2yr7fyf4"))  # -> "Needs
+    # the second algorithm!"
