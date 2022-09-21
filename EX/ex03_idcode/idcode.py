4bfe9f05003b1e31098b12e63c0b1466272bc4ae
igmark <igmark@ttu.ee>
2022-09-21 12:25:42 +0300

    asdy

--- EX/ex03_idcode/idcode.py
+++ EX/ex03_idcode/idcode.py
@@ -189,7 +189,7 @@ def get_data_from_id(id_code: str) -> str:
         month_number = int(id_code[3:5])
         birth_place = int(id_code[7:10])
         full_year = get_full_year(gender_number, year_number)
-        return f"This is a {get_gender(gender)} born on {day_number}.{month_number}.{get_full_year(full_year)} in {get_birth_place(birth_place)}"
+        return f"This is a {get_gender(gender)} born on {day_number}.{month_number}.{full_year} in {get_birth_place(birth_place)}"
     else:
         return "Given invalid ID code!"
 
