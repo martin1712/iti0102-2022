e011f85910feb5a35f561c39ec2a745af845fe35
Martin <markovigor99@gmail.com>
2022-09-24 17:12:26 +0300

    3 try

--- EX/ex04_lists/lists.py
+++ EX/ex04_lists/lists.py
@@ -26,13 +26,20 @@ def car_models(all_cars: str) -> list:
     a = all_cars.split(",")
     for car in a:
         tokens = car.split(" ")
-        if tokens[1] not in all_makes:
-            all_makes.append(tokens[1])
-        if all_cars == "":
-            return []
+        if len(tokens) > 0:
+            if tokens[1] not in all_makes:
+                all_makes.append(tokens[1])
+            if all_cars == "":
+                return []
     return all_makes
 
 
-print(car_makes("Audi A4,Skoda Super,Skoda Octavia,BMW 530,Seat Leon,Skoda Superb,Skoda Superb,BMW x5"))
+print(list_of_cars("Audi A4,Skoda Superb,Audi A4"))  # ["Audi A4", "Skoda Superb", "Audi A4"]
+
+print(car_makes("Audi A4,Skoda Super,Skoda Octavia,BMW 530,Seat Leon,Skoda Superb,Skoda Superb,BMW x5"))  # ['Audi', 'Skoda', 'BMW', 'Seat']
+
+print(car_makes("Mazda 6,Mazda 6,Mazda 6,Mazda 6"))  # ['Mazda']
+
 print(car_makes(""))  # []
+
 print(car_models("Audi A4,Skoda Superb,Audi A4,Audi A6"))  # ["A4", "Superb", "A6"]
