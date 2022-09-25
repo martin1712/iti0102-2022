c44958881b81fe90a2d0d56ddfdcd59e77b67a01
Martin <markovigor99@gmail.com>
2022-09-25 14:10:02 +0300

    3 try

--- EX/ex04_lists/lists.py
+++ EX/ex04_lists/lists.py
@@ -25,13 +25,38 @@ def car_models(all_cars: str) -> list:
     all_makes = []
     a = all_cars.split(",")
     for car in a:
-        tokens = car.split(" ")
+        tokens = car.split(" ", 1)
         if 1 < len(tokens):
             if tokens[1] not in all_makes:
                 all_makes.append(tokens[1])
         if all_cars == "":
             return []
     return all_makes
+
+
+def search_by_make(all_cars: str) -> list:
+    all_makes = []
+    a = all_cars.split(",")
+    for car in a:
+        tokens = car.split(" ")
+        if tokens[0] in all_cars:
+            all_makes.append(tokens[0])
+        if all_cars == "":
+            return []
+    return a
+
+
+def search_by_model(all_cars: str) -> list:
+    all_makes = []
+    a = all_cars.split(",")
+    for car in a:
+        tokens = car.split(" ")
+        if tokens[1] in all_cars and 1 < len(tokens):
+            all_makes.append(tokens[1])
+        if all_cars == "":
+            return []
+    return a
+
 
 
 print(list_of_cars("Audi A4,Skoda Superb,Audi A4"))  # ["Audi A4", "Skoda Superb", "Audi A4"]
@@ -42,4 +67,8 @@ print(car_makes("Mazda 6,Mazda 6,Mazda 6,Mazda 6"))  # ['Mazda']
 
 print(car_makes(""))  # []
 
-print(car_models("Audi A4,Skoda Superb,Audi A4,Audi A6"))  # ["A4", "Superb", "A6"]
+print(car_models("Audi A4 SUPER,Skoda Superb,Audi A4,Audi A6"))  # ["A4", "Superb", "A6"]
+
+print(search_by_make("Audi A4,audi A5,AUDI a6 A7"))
+
+print(search_by_model("Audi A4,Audi a4 2021,Audi A40"))
