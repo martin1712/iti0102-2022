728322175dff0119e6bccf98d4a42e4145e866d3
Martin <markovigor99@gmail.com>
2022-10-09 15:15:58 +0300

    1 try

---
+++ EX/ex06_regex/regex.py
@@ -0,0 +1,48 @@
+
+
+"""Regex, yay."""
+import re
+
+
+
+def find_words(text: str) -> list:
+    words = re.findall(r"[ÕÜÖÄ, A-Z]+[a-z]+", text)
+    return words
+
+
+def find_words_with_vowels(text: str) -> list:
+    words = re.findall(r"[AUEOIÕÜÖÄ]+[a-z]+", text)
+    return words
+
+
+def find_sentences(text: str) -> list:
+    words = re.findall(r"([A-Z][^\.!?]*[\.!?])", text)
+    return words
+
+
+def find_words_from_sentence(sentence: str) -> list:
+    pass
+
+
+def find_words_from_sentences_only(text: str) -> list:
+
+    pass
+
+
+def find_years(text: str) -> list:
+
+    pass
+
+
+def find_phone_numbers(text: str) -> dict:
+
+    pass
+
+
+if __name__ == '__main__':
+    print(find_words('KanaMunaPelmeen!!ApelsinÕunMandariinKakaoHernesAhven'))
+    # ['Kana', 'Muna', 'Pelmeen', 'Apelsin', 'Õun', 'Mandariin', 'Kakao', 'Hernes', 'Ahven']
+    print(find_words_with_vowels('KanaMunaPelmeenApelsinÕunMandariinKakaoHernesAhven'))
+    # ['Apelsin', 'Õun', 'Ahven']
+    print(find_sentences('See on esimene - lause. See on ä teine lause! see ei ole lause. Aga kas see on? jah, oli.'))
+    # ['See on esimene - lause.', 'See on ä teine lause!', 'Aga kas see on?']
