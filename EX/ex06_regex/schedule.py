7b4be8d695fe5e2739a3ee6ed7788c450469803c
Martin <markovigor99@gmail.com>
2022-10-12 14:31:44 +0300

    2 try

--- EX/ex06_regex/schedule.py
+++ EX/ex06_regex/schedule.py
@@ -2,7 +2,7 @@
 
 """Create schedule from the given file."""
 import re
-
+import datetime
 
 def create_schedule_file(input_filename: str, output_filename: str) -> None:
     """Create schedule file from the given input file."""
@@ -13,9 +13,10 @@ def create_schedule_string(input_string: str) -> str:
     """Create schedule string from the given input string."""
     together = {}
     for match in re.finditer(r"((\d\d.\d\d)|(\d\d.\d)|(\d.\d\d)|(\d.\d))\s+([a-z]+|[A-Z][a-z]+)", input_string):
-
-
-        together[match.group(1)] = match.group(6)
+        a = match.group(1).split(":")
+        if int(a[0]) <= 23 and int(a[1]) <= 59:
+            d = datetime.datetime.strptime(match.group(1), "%H:%M")
+            together[d.strftime("%I:%M %p")] = match.group(6)
         print(together)
     for i in together:
 
@@ -25,5 +26,5 @@ def create_schedule_string(input_string: str) -> str:
 
 
 if __name__ == '__main__':
-    print(create_schedule_string("wat 11:00 teine tekst 11:0 jah ei 10:00 pikktekst "))
+    print(create_schedule_string("wat 11:00 teine tekst 11:0 jah ei 10:00 pikktekst 23:45 ok 01:01 okidoki 55:444 wow "))
     create_schedule_file("schedule_input.txt", "schedule_output.txt")
