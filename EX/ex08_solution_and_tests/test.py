c674e9597c65b841d7f7b302feed032fbc72363c
Martin <markovigor99@gmail.com>
2022-10-23 18:48:27 +0300

    1 try

--- EX/ex08_solution_and_tests/test.py
+++ EX/ex08_solution_and_tests/test.py
@@ -7,8 +7,8 @@ from solution import students_study
 def test_students_study_evening_no_coffe_needed():
     assert students_study(20, True) is True
     assert students_study(20, False) is True
-    assert students_study(24, True) is True
-    assert students_study(18, False) is True
+    assert students_study(1, True) is False
+    assert students_study(17, False) is False
 
 
 def test_students_study_night_no_study():
