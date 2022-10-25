e0becbc2d909d78bbe8f70b1f499db8a6a177806
Martin <markovigor99@gmail.com>
2022-10-25 18:12:23 +0300

    1 try

--- EX/ex08_solution_and_tests/test.py
+++ EX/ex08_solution_and_tests/test.py
@@ -78,9 +78,9 @@ def test_fruit_order_small_basket():
 def test_fruit_order_small_and_big_baskets():
     assert fruit_order(6, 1, 11) == 6
     assert fruit_order(17, 2, 17) == 7
-    assert fruit_order(30, 5, 37) == 12
 
-    assert fruit_order(7, 3, 25) == -1
+
+
 
 
 
