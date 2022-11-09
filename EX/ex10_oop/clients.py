26cf53ec538d77e4bbb8a7e6d50bf903c52bc017
Martin <markovigor99@gmail.com>
2022-11-09 17:10:04 +0300

    1 try

--- EX/ex10_oop/clients.py
+++ EX/ex10_oop/clients.py
@@ -100,8 +100,8 @@ def largest_loss_per_day(filename: str) -> Optional[Client]:
 if __name__ == '__main__':
     print(read_from_file_into_list("clients_info.txt"))  # -> [Ann, Mark, Josh, Jonah, Franz]
 
-    print(filter_by_bank("clients_info.txt", "Sprint"))  # -> [Ann, Mark]
+    # print(filter_by_bank("clients_info.txt", "Sprint"))  # -> [Ann, Mark]
 
-    print(largest_earnings_per_day("clients_info.txt"))  # -> Josh
+    # print(largest_earnings_per_day("clients_info.txt"))  # -> Josh
 
-    print(largest_loss_per_day("clients_info.txt"))  # -> Franz
+    # print(largest_loss_per_day("clients_info.txt"))  # -> Franz
