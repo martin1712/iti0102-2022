0d89e8b18b41aa917244e0309c4e25a21b7ceb88
Martin <markovigor99@gmail.com>
2022-11-21 17:45:19 +0300

    1 try

--- EX/ex12_adventure/adventure.py
+++ EX/ex12_adventure/adventure.py
@@ -21,16 +21,15 @@ class Adventurer:
 
     def add_power(self, power: int):
         """Power."""
-        return self.power + power
+        self.power = self.power + power
+
 
     def add_experience(self, exp: int):
         """Exp."""
-        result = self.experience + exp
-        if result > 99:
-            self.power = self.power + result // 10
+        if self.experience > 99:
+            self.power = self.power + self.experience // 10
             self.experience = 0
-        return result
-
+        self.experience = self.experience + exp
 
 class World:
     pass
@@ -41,5 +40,20 @@ class Monster:
 
 
 if __name__ == "__main__":
-    hero = Adventurer("Toots", "the Fighter", 10)
-    print(hero)
+    hero = Adventurer("Sander", "Paladin", 50)
+    friend = Adventurer("Peep", "Druid", 25)
+    another_friend = Adventurer("Toots", "Wizard", 40)
+    annoying_friend = Adventurer("XxX_Eepiline_Sõdalane_XxX", "Tulevikurändaja ja ninja", 999999)
+
+    print(hero)  # -> "Sander, the Paladin, Power: 50, Experience: 0."
+    # Ei, tüütu sõber, sa ei saa olla tulevikurändaja ja ninja, nüüd sa pead fighter olema.
+    # Ei maksa liiga tugevaks ka ennast alguses teha!
+    print(annoying_friend)  # -> "XxX_Eepiline_Sõdalane_XxX, the Fighter, Power: 10, Experience: 0."
+    print(friend)  # -> "Peep, the Druid, Power: 25, Experience: 0."
+    print(another_friend)  # -> "Toots, the Wizard, Power: 40, Experience: 0."
+    print()
+
+    print("Peep, sa tundud kuidagi nõrk, ma lisasin sulle natukene tugevust.")
+    friend.add_power(20)
+    print(friend)  # -> "Peep, the Druid, Power: 45, Experience: 0."
+    print()
