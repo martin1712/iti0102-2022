30005a3cfec7dbbf3d7e34126ece7dbcd3fc00ca
Martin <markovigor99@gmail.com>
2022-11-27 17:54:16 +0300

    1 try

--- EX/ex13_api/google_api.py
+++ EX/ex13_api/google_api.py
@@ -24,8 +24,8 @@ def get_links_from_spreadsheet(id: str, token_file_name: str) -> list:
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists("token.json"):
-        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    if os.path.exists(token_file_name):
+        creds = Credentials.from_authorized_user_file(token_file_name, SCOPES)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
@@ -35,7 +35,7 @@ def get_links_from_spreadsheet(id: str, token_file_name: str) -> list:
                 'credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open("token.json", 'w') as token:
+        with open(token_file_name, 'w') as token:
             token.write(creds.to_json())
 
     try:
