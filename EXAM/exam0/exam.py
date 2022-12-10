2b9b50b750dcb40c4f791f489388f39f8d4255b0
Martin <markovigor99@gmail.com>
2022-12-10 20:36:07 +0300

    1 try

--- EXAM/exam0/exam.py
+++ EXAM/exam0/exam.py
@@ -21,54 +21,67 @@ game = [[1, 2, 1], [2, 1, 2], [2, 1, 0]]
 
 
 def tic_tac_toe(game: list) -> int:
+    """
+    Find game winner.
 
-    if game[0][0] != 0:
+    #4
 
-        if game[0][0] == game[0][1] == game[0][2]:
-            return game[0][0]
+    The 3x3 table is represented as a list of 3 rows, each row has 3 element (ints).
+    The value can be 1 (player 1), 2 (player 2) or 0 (empty).
+    The winner is the player who gets 3 of her pieces in a row, column or diagonal.
 
-        elif game[0][0] == game[1][1] == game[2][2]:
-            return game[0][0]
+    There is only one winner or draw. You don't have to validate whether the game is in correct (possible) state.
+    I.e the game could have four 1s and one 0 etc.
 
-        elif game[0][0] == game[1][0] == game[2][0]:
-            return game[0][0]
-        else:
-            return 0
+    tic_tac_toe([[1, 2, 1], [2, 1, 2], [2, 2, 1]]) => 1
+    tic_tac_toe([[1, 0, 1], [2, 1, 2], [2, 2, 0]]) => 0
+    tic_tac_toe([[2, 2, 2], [0, 2, 0], [0, 1, 0]]) => 2
 
-    elif game[1][0] != 0:
+    :param game
+    :return: winning player id
+    """
+    for i in game:
+        if 0 not in i:
+            if sum(i) // 3 == 1:
+                return 1
+            else:
+                return 2
+    if game[0][0] and game[1][1] and game[2][2] != 0 and game[0][0] + game[1][1] + game[2][2] == 3:
+        return 1
+    if game[0][0] and game[1][1] and game[2][2] != 0 and game[0][0] + game[1][1] + game[2][2] == 6:
+        return 2
+    if game[0][0] and game[1][0] and game[2][0] != 0 and game[0][0] + game[1][0] + game[2][0] == 3:
+        return 1
+    if game[0][0] and game[1][0] and game[2][0] != 0 and game[0][0] + game[1][0] + game[2][0] == 6:
+        return 2
+    if game[0][1] and game[1][1] and game[2][1] != 0 and game[0][0] + game[1][1] + game[2][1] == 3:
+        return 1
+    if game[0][1] and game[1][1] and game[2][1] != 0 and game[0][0] + game[1][1] + game[2][1] == 6:
+        return 2
+    if game[0][2] and game[1][2] and game[2][2] != 0 and game[0][2] + game[1][2] + game[2][2] == 3:
+        return 1
+    if game[0][2] and game[1][2] and game[2][2] != 0 and game[0][2] + game[1][2] + game[2][2] == 6:
+        return 2
+    return 0
 
-        if game[1][0] == game[1][1] == game[1][2]:
-            return game[1][0]
-        else:
-            return 0
 
-    elif game[2][0] != 0:
 
-        if game[2][0] == game[2][1] == game[2][2]:
-            return game[2][0]
-        return 0
 
-    elif game[0][2] != 0:
 
-        if game[0][2] == game[1][1] == game[2][0]:
-            return game[0][2]
-        return 0
 
-    elif game[0][1] != 0:
 
-        if game[0][1] == game[1][1] == game[2][1]:
-            return game[0][1]
-        return 0
 
-    elif game[0][2] != 0:
 
-        if game[0][2] == game[1][2] == game[2][2]:
-            return game[0][2]
-        return 0
 
-    else:
-        return 0
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
-    print(tic_tac_toe([[0, 0, 0], [1, 1, 1], [0, 0, 0]]))
+    print(tic_tac_toe([[1, 1, 0],
+                       [0, 2, 0],
+                       [1, 1, 0]]))
